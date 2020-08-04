b7926c292a4627601f5afa84179000063db04b33
madhatter <matthew.rikard@gmail.com>
2020-08-04 03:08:23 -0400

    added api for current weather

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // api key 0218f2702681a533f2e51a253c19faab
 $(document).ready(function () {
     var city = "paramus"
-            $.ajax({
-                method: "GET",
-                url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=0218f2702681a533f2e51a253c19faab',
-            }).then(function(response){
-                console.log(response);
-                console.log(response.list[0].main.temp);
-            });
+        $.ajax({
+            method: "GET",
+            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=0218f2702681a533f2e51a253c19faab',
+        }).then(function(response){
+            console.log(response);
+            console.log(response.list[0].main.temp);
         });
+    
+
+        $.ajax({
+            method: "GET",
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=0218f2702681a533f2e51a253c19faab'
+
+        }).then(function(response){
+            console.log(response);
+            console.log(response.main.temp);
+        });
+});
