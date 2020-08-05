78b519c7f34e42ac54b7946b2d935c86c1508d8b
madhatter <matthew.rikard@gmail.com>
2020-08-04 22:03:43 -0400

    many changes after power outage

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // api key 0218f2702681a533f2e51a253c19faab
 $(document).ready(function() {
-    $("#submitSearch").on("click", function(){
-        var city = $("#city").val();
-        if(city != ""){
-            $.ajax({
-            method: "GET",
-            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=0218f2702681a533f2e51a253c19faab', 
-            }).then(function(response){
-            console.log(response);
-            console.log(response.list[0].main.temp);
-            });
-        };
-    });
+  
     function clock() {
         var time = moment().format('h:mm:ss a');
         var date = moment().format('MMMM Do YYYY');
@@ -19,17 +8,54 @@ $(document).ready(function() {
         $('#date').text(date);
     }
         setInterval(clock, 1000);
-        
-       
 
+    var savedCities = ['NEW YORK', 'PARAMUS'];
+    function getCities(){
+        var storedCity = JSON.parse(localStorage.getItem('searchHistory'));
+        if (storedCity !== null){
+            savedCities = storedCity;
+        }
+        addBtn();
+    }
+    function addBtn(){
+        $('#cityBtns').html('');
+        for (var i=0; i < savedCities.length; i++){
+            var city = savedCities[i];
+            var createBtn = $('<button>');
+            createBtn.addClass('btn btn-success btn-outline-dark');
+            createBtn.attr('data-name', city);
+            createBtn.attr('id', 'cityBtns');
+            createBtn.text(city);
+            $('#cityBtnsDiv').prepend(createBtn);
+        }
+    }
     
+    $("#submitSearch").on("click", function() {
+        var citySearchText = ($("#city").val().toUpperCase());
+        if (citySearchText === "") {
+            return;
+        }
+        savedCities.push(citySearchText);
+        localStorage.setItem("searchHistory", JSON.stringify(savedCities));
+        $("#city").html('');
+        addBtn();
+    });
 
-//         $.ajax({
-//             method: "GET",
-//             url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=0218f2702681a533f2e51a253c19faab'
+    $('#cityBtns').on('click', function(){
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q='
+        var city = $(this).attr('#data-name')
+        console.log(city);
+        var apiKey = '&units=imperial&appid=0218f2702681a533f2e51a253c19faab'
+        $.ajax({
+            method: 'GET',
+            url: url + city + apiKey
+        }).then(function(response){
+            console.log(response);
+            console.log(response.main.temp)
+        });
 
-//         }).then(function(response){
-//             console.log(response);
-//             console.log(response.main.temp);
-//         });
-})
+        
+
+    });
+        getCities();
+    })
